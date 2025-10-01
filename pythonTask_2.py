e34e2b8298f0641fce53c21c68cf4227c0450d99
yakir <yakir5814@gmail.com>
2025-10-01 15:44:06 +0300

    Task2.

---
+++ pythonTask_2.py
@@ -0,0 +1,13 @@
+import statistics
+
+def calculate_statistics(numbers):
+    mean = statistics.mean(numbers)
+    median = statistics.median(numbers)
+    mode = statistics.mode(numbers)
+    return mean, median, mode
+# Test the function
+numbers = [10, 20, 20, 30, 40]
+mean, median, mode = calculate_statistics(numbers)
+print(f"mean: {mean}")
+print (f"Median: {median}")
+print (f"Mode:{mode}")
