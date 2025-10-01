c2a9c737c9819ac0372b1d8e6fc62c2ae6090afb
yakir <yakir5814@gmail.com>
2025-10-01 14:48:12 +0300

    WoW

---
+++ jb_factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+#Test the factorial function
+number = 
+print(f"Factorial of {number} is: {factorial(number)}")
