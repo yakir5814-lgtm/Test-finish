09a512ced732f97b373a5797ff60abe0adbc6089
yakir <yakir5814@gmail.com>
2025-10-02 10:57:48 +0300

    NEW11

--- python_basics.py
+++ python_basics.py
@@ -4,7 +4,7 @@ b = int(input("how many green: "))
 c = int(input("how many yellow: "))
 d = int(input("how many red: "))
 
-print(f"the calculeter all Things is : {a+b+c+d} Shekel")
+print(f"the calculeter all Things is : {a+b+c+d} Shekel")111
 
 
 
